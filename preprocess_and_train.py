982e0340709501f4ebd34c568f6a47972c145873
Comyar Dehlavi <62218304+Void-Overflow@users.noreply.github.com>
2025-07-17 10:17:12 -0500

    Update preprocess_and_train.py

--- preprocess_and_train.py
+++ preprocess_and_train.py
@@ -7,6 +7,7 @@
 
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
